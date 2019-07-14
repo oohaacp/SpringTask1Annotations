ab0725c99562f4edf68e85068555fb085c227fba
oohaacp <oohaareddy@gmail.com>
2019-07-14 22:24:29 +0530

    aware interfaceis finished

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,9 +1,15 @@
 package com.stackroute.domain;
 import com.stackroute.domain.Actor;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
-public class Movie
+public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNameAware
 {
     @Autowired
     public Actor actor;
@@ -34,4 +40,21 @@ public class Movie
         System.out.println("Actor information : "+actor.getName()+" : gender is :" +actor.getGender()+" : age is :" +actor.getAge());
     }
 
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException
+    {
+        System.out.println("Implementing BeanFactory");
+    }
+
+    @Override
+    public void setBeanName(String s)
+    {
+        System.out.println("Implementing Aware");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException
+    {
+        System.out.println("Implementing ApplicationContextAware");
+    }
 }
